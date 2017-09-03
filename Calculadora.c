18a53ec951038d4b6fbe0cbc0ba247a7845e6181
sebastian López Fernández <sebastian Lpez Fernndez>
2017-09-03 09:54:42 -0500

    Programa con comentarios

--- Calculadora.c
+++ Calculadora.c
@@ -8,55 +8,54 @@
 
 #include <xc.h>
 #include "config.h"
-#include <stdio.h>
+#include <stdio.h> //libreria de matematica
 #include <math.h>
-
-float a = 10;
-float b = 5; 
-float resultado, suma, resta, multiplicacion, division;
+//*********** variables a usar para la calculadora ************
+float a = 10; //damos el valor a la primera variable
+float b = 0;  //damos el segundo valor a la segunda Variable
+float suma, resta, multiplicacion, division; // nombre de las variables para efectuar las operaciones
+///*************************************************************
 //int opcion;
 
 void main(void) {
     
-    TRISB = 0b11111111;
-    PORTB = 0;    
+    TRISB = 0b11111111; //Declara el puerto B como entrada
+    PORTB = 0;    //Limpia el Puerto B
     while (1) {      
         
-    iniciar_uart();
-    printf("************************************\n");
-    printf("******      1. Suma      *********\n");
-    printf("******      2. Resta     *********\n");
-    printf("*****  3. Multiplicacion  ********\n");
-    printf("******     4. Division   *********\n");
-    printf("************************************\n");   
+    iniciar_uart(); //inicia el modulo usart para que lo podamos ver en pantalla
+    //**************** Da a escojer el usuario la Operacion...
+    printf("****************************************\n"); 
+    printf("******      1.(RB0). Suma      *********\n");
+    printf("******      2.(RB1). Resta     *********\n");
+    printf("*****  3.(RB2). Multiplicacion  ********\n");
+    printf("******     4.(RB3). Division   *********\n");
+    printf("****************************************\n");   
+    ///*********************** ...que se quiere utilizar, y que aparece como un Menu
     
-    suma = a+b;
-    resta = a-b;
-    multiplicacion = a*b;
-    division =  a/b;
+    suma = a+b; //hace la suma de las 2 variables
+    resta = a-b; //hace la resta de las 2 variables
+    multiplicacion = a*b; //hace la multiplicación de las 2 variables
+    division =  a/b; //hace la División de las 2 variables
     
-    if (PORTBbits.RB0 == 1){        
-        printf("El resultado de la Suma es: %f\n", suma);
+    if (PORTBbits.RB0 == 1){        // pregunta si se ha presionado RB0, 
+        printf("El resultado de la Suma es: %f\n", suma); //de ser asi muestra en pantalla el valor de la Suma
     }
-    if (PORTBbits.RB1 == 1){        
-        printf("El resultado de la Resta es: %f\n", resta);
+    if (PORTBbits.RB1 == 1){        // pregunta si se ha presionado RB0,
+        printf("El resultado de la Resta es: %f\n", resta); //de ser asi muestra en pantalla el valor de la Suma
         
     }
-    if (PORTBbits.RB2 == 1){        
-        printf("El resultado de la Multiplicacion es: %f\n", multiplicacion);        
+    if (PORTBbits.RB2 == 1){        // pregunta si se ha presionado RB0,
+        printf("El resultado de la Multiplicacion es: %f\n", multiplicacion);        //de ser asi muestra en pantalla el valor de la Suma
     }
-    if (PORTBbits.RB3 == 1){
-        if (b==0){
-            printf("Error, no es posible dividir entre 0.\n");            
+    if (PORTBbits.RB3 == 1){    // pregunta si se ha presionado RB0,
+        if (b==0){              //en caso de que la variable b, que es la que esta dividiendo, sea 0
+            printf("Error, no es posible dividir entre 0.\n");            //imprime este en pantalla "Error"
             }
-        else {                    
-        resultado = a/b;
-        printf("El resultado de la Division es: %f\n", division);
+        else {                    //en caso de que la variable b no sea 0, 
+            printf("El resultado de la Division es: %f\n", division); // muestra en pantalla el valor de la División
         }
-    }
-    else {
-        printf("Opcion Incorrecta\n ");
-    }
+    }    
     
-   }
+   } 
 }
